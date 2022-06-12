37e7ad021edb2d719a3d019426d149d496a35eba
Mridul2820 <immridulpanda2820@gmail.com>
2022-06-12 18:37:08 +0530

    demo set

--- src/App.js
+++ src/App.js
@@ -1,39 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Repository } from './lib';
 
 const App = () => {
+  const [username, setUsername] = useState('facebook');
+  const [repository, setRepository] = useState('react');
+  const [theme, setTheme] = useState('light');
+  const [showStarCount, setShowStarCount] = useState(true);
+  const [showForksCount, setShowForksCount] = useState(true);
+  const [showLanguage, setShowLanguage] = useState(true);
+  const [showDescription, setShowDescription] = useState(true);
+  const [showType, setShowType] = useState(true);
+
   return (
-    <div className="flex flex-wrap gap-5 p-5">
-      <Repository
-        username="facebook"
-        repository="react"
-        theme="light"
-        showStarCount={true}
-        showForkCount={true}
-        showLanguage={true}
-        showDescription={true}
-        showType={true}
-      />
-      <Repository
-        username="Mridul2820"
-        repository="cineparadis"
-        theme="light"
-      />
-      <Repository
-        username="Mridul2820"
-        repository="css-js"
-        theme="light"
-        showStarCount={true}
-        showForkCount={true}
-        showLanguage={true}
-        showDescription={true}
-        showType={true}
-      />
-      <Repository
-        username="Mridul2820"
-        repository="next-template"
-        theme="dark"
-      />
+    <div className="flex flex-col lg:flex-row-reverse justify-center gap-10 p-5">
+      <div className="flex flex-col gap-8">
+        <Repository
+          username={username}
+          repository={repository}
+          theme={theme}
+          showStarCount={showStarCount}
+          showForkCount={showForksCount}
+          showLanguage={showLanguage}
+          showDescription={showDescription}
+          showType={showType}
+        />
+        <div className="">
+          <h2>Code</h2>
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <h1 className="font-bold text-lg">React Github Embade Playground</h1>
+        <div className="input-row">
+          <label className="text-gray-600">Username</label>
+          <input
+            className="input"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="input-row">
+          <label className="text-gray-600">Repository</label>
+          <input
+            className="input"
+            value={repository}
+            onChange={(e) => setRepository(e.target.value)}
+          />
+        </div>
+        <div className="input-row">
+          <label className="text-gray-600">Theme</label>
+          <select
+            className="input"
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+          >
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+          </select>
+        </div>
+        <div className="input-row">
+          <label className="text-gray-600">Show Star Count</label>
+          <input
+            className="input"
+            type="checkbox"
+            checked={showStarCount}
+            onChange={() => setShowStarCount(!showStarCount)}
+          />
+        </div>
+        <div className="input-row">
+          <label className="text-gray-600">Show Fork Count</label>
+          <input
+            className="input"
+            type="checkbox"
+            checked={showForksCount}
+            onChange={() => setShowForksCount(!showForksCount)}
+          />
+        </div>
+        <div className="input-row">
+          <label className="text-gray-600">Show Language</label>
+          <input
+            className="input"
+            type="checkbox"
+            checked={showLanguage}
+            onChange={() => setShowLanguage(!showLanguage)}
+          />
+        </div>
+        <div className="input-row">
+          <label className="text-gray-600">Show Description</label>
+          <input
+            className="input"
+            type="checkbox"
+            checked={showDescription}
+            onChange={() => setShowDescription(!showDescription)}
+          />
+        </div>
+        <div className="input-row">
+          <label className="text-gray-600">Show Type</label>
+          <input
+            className="input"
+            type="checkbox"
+            checked={showType}
+            onChange={() => setShowType(!showType)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
